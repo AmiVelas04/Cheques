5aa0fe5076ed6d8b0cccf46ec5a30ce81d7000ac
AmiVelas04 <Jose.velas04@outlook.com>
2021-05-12 00:16:41 -0600

    cheque impresos e inicio de creacion de reportes

---
+++ Modelos/reportesModelo.php
@@ -0,0 +1,17 @@
+<?php 
+
+ if($peticionajax) {
+
+require_once "../core/modeloMain.php";
+	
+}
+else
+{
+require_once "./core/modeloMain.php";
+
+}
+
+
+class reportesModelo extends modeloMain
+{
+}
--- Vistas/Modulos/navlateral.php
+++ Vistas/Modulos/navlateral.php
@@ -1,4 +1,3 @@
-
 <!-- SideBar -->
 	<section class="full-box cover dashboard-sideBar">
 		<div class="full-box dashboard-sideBar-bg btn-menu-dashboard"></div>
@@ -10,27 +9,26 @@
 			<div class="full-box text-uppercase text-center text-titles dashboard-sideBar-title">
 				 <i class="zmdi zmdi-close btn-menu-dashboard visible-xs"> </i>
 			</div>
-
 			<!-- SideBar User info -->
 			<div class="full-box dashboard-sideBar-UserInfo">
 				<figure class="full-box">
-					<img src="<?php echo SERVERURL; ?>vistas/assets/avatars/<?php echo $_SESSION['foto_sbp'] ?>" alt="UserIcon">
+					<img src="<?php echo SERVERURL; ?>vistas/assets/avatars/<?php echo $_SESSION['foto_sbp']?>" alt="UserIcon">
 					<figcaption class="text-center text-titles"><?php 
-					echo $_SESSION['usuario']; ?></figcaption>
+					echo $_SESSION['usuario'];?></figcaption>
 				</figure>
 				<ul class="full-box list-unstyled text-center">
 					<li>
-						<a href=" <?php echo SERVERURL; ?>misdatos" title="Mis datos">
+						<a href=" <?php echo SERVERURL;?>misdatos" title="Mis datos">
 							<i class="zmdi zmdi-account-circle"></i>
 						</a>
 					</li>
 					<li>
-						<a href=" <?php echo SERVERURL; ?>micuenta" title="Mi cuenta">
+						<a href=" <?php echo SERVERURL;?>micuenta" title="Mi cuenta">
 							<i class="zmdi zmdi-settings"></i>
 						</a>
 					</li>
 					<li>
-						<a href="<?php echo $lc->encryption($_SESSION['token_pyc']); ?>" title="Salir del sistema" class="btn-exit-system">
+						<a href="<?php echo  $lc->encryption($_SESSION['token_pyc']);?>" title="Salir del sistema" class="btn-exit-system">
 							<i class="zmdi zmdi-power"></i>
 						</a>
 					</li>
@@ -39,28 +37,25 @@
 			<!-- SideBar Menu -->
 			<ul class="list-unstyled full-box dashboard-sideBar-Menu">
 			<li>
-					<a href="<?php echo SERVERURL; ?>main"" class="">
+					<a href="<?php echo SERVERURL;?>main"" class="">
 						<i class="zmdi zmdi-store zmdi-hc-fw"></i> Página Principal <i class=""></i>
 					</a>
 			
 				</li>	
-
-
 				<li>
 					<a href="#!" class="btn-sideBar-SubMenu">
 						<i class="zmdi zmdi-case zmdi-hc-fw"></i> Cheques <i class="zmdi zmdi-caret-down pull-right"></i>
 					</a>
 					<ul class="list-unstyled full-box">
 						<li>
-							<a href=" <?php echo SERVERURL; ?>cheques"><i class="zmdi zmdi-plus-square zmdi-hc-fw"></i> Generar Cheque</a>
+							<a href=" <?php echo SERVERURL;?>cheques"><i class="zmdi zmdi-plus-square zmdi-hc-fw"></i> Generar Cheque</a>
 						</li>
 						
 						<li>
-							<a href=" <?php echo SERVERURL; ?>liberarch"><i class="zmdi zmdi-money zmdi-hc-fw"></i>Liberar Cheque</a>
+							<a href=" <?php echo SERVERURL;?>liberarch"><i class="zmdi zmdi-money zmdi-hc-fw"></i>Liberar Cheque</a>
 						</li>
 				</ul>
 				</li>
-				
 				<li>
 				<li>
 					<a href="#!" class="btn-sideBar-SubMenu">
@@ -68,22 +63,20 @@
 					</a>
 					<ul class="list-unstyled full-box">
 						<li>
-							<a href=" <?php echo SERVERURL; ?>usuario"><i class="zmdi zmdi-account-add zmdi-hc-fw"></i>Usuarios</a>
+							<a href=" <?php echo SERVERURL;?>usuario"><i class="zmdi zmdi-account-add zmdi-hc-fw"></i>Usuarios</a>
 						</li>
-						 
-						<li>
-							<a href=" <?php echo SERVERURL; ?>ingresoch"><i class="zmdi zmdi-card zmdi-hc-fw"></i>Crear Chequeras</a>
+						 <li>
+							<a href=" <?php echo SERVERURL;?>ingresoch"><i class="zmdi zmdi-card zmdi-hc-fw"></i>Crear Chequeras</a>
 						</li>
-						
-						
 					</ul>
 				</li>
-		
+
 				<li>
-					<a href=" <?php echo SERVERURL; ?>addcali">
+					<a href="<?php echo SERVERURL;?>">
 						<i class="zmdi zmdi-book-image zmdi-hc-fw"></i> Reportes
 					</a>
 				</li>
+				
 			</ul>
 		</div>
 	</section>
